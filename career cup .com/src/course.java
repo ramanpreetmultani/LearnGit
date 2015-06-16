e2390e37caa2f52e7bd3ac6f22d03945c1402d65
Your Name <johndoe@example.com>
2015-06-16 17:38:50 +0530

    course.java

---
+++ career cup .com/src/course.java
@@ -0,0 +1,11 @@
+import java.util.Queue;
+
+
+public class course {
+	//all these details should be public
+		String name;
+		String semester;//fall spring both
+		int dependentOn;
+		String dependentOnCourses[];
+		Queue<course> dependentCourses;
+}
