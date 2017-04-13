3255b323d0d3e9ec9e3163203ff9def7bf32e49b
amitgarg3132 <amitgarg3132@gmail.com>
2017-04-13 19:01:34 +0530

    Update course.java

--- career cup .com/src/course.java
+++ career cup .com/src/course.java
@@ -3,7 +3,7 @@ import java.util.Queue;
 
 public class course {
 	//all these details should be public
-		String name;
+		String name;//git changes to understand what happened
 		String semester;//fall spring both
 		int dependentOn; athrv ad 
 		String dependentOnCourses[];
