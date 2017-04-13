c5f407dc869407476eb981024c0257420f45de5e
a0g00dw <a0g00dw@LEUS28233316279.homeoffice.Wal-Mart.com>
2017-04-13 19:17:22 +0530

    adf

--- career cup .com/src/course.java
+++ career cup .com/src/course.java
@@ -3,11 +3,7 @@ import java.util.Queue;
 
 public class course {
 	//all these details should be public
-<<<<<<< HEAD
 		String name; // understand local changes
-=======
-		String name;//git changes to understand what happened
->>>>>>> branch 'master' of https://github.com/amitgarg3132/Algorithm-in-Java.git
 		String semester;//fall spring both
 		int dependentOn; abc
 		String dependentOnCourses[];
