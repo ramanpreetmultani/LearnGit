df4da9f1adb04705eab0816c683f532dfb558a40
a0g00dw <a0g00dw@LEUS28233316279.homeoffice.Wal-Mart.com>
2017-04-13 19:02:09 +0530

    local change

--- career cup .com/src/course.java
+++ career cup .com/src/course.java
@@ -3,7 +3,7 @@ import java.util.Queue;
 
 public class course {
 	//all these details should be public
-		String name; 
+		String name; // understand local changes
 		String semester;//fall spring both
 		int dependentOn; abc
 		String dependentOnCourses[];
